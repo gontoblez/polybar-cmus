5ff34d3473d5b637814cc269176cef7efad1cc62
gontoblez <nad-git@outlook.com>
2023-03-07 19:50:36 +0300

    Added function 'player' to print song info

--- cmus.sh
+++ cmus.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 prepend_zero () {
-        seq -f "%02g" $1 $1
+    seq -f "%02g" $1 $1
+}
+
+player () {
+    echo -n "$artist - $song [$minutes1:$seconds1/$minutes2:$seconds2] "
 }
 
 artist=$(echo -n $(cmus-remote -C status | grep "tag artist" | cut -c 12-))
@@ -14,7 +18,9 @@ if [[ $artist = *[!\ ]* ]]; then
         duration=$(cmus-remote -C status | grep duration | cut -c 10-)
         minutes2=$(prepend_zero $(($duration / 60)))
         seconds2=$(prepend_zero $(($duration % 60)))
-        echo -n "$artist - $song [$minutes1:$seconds1/$minutes2:$seconds2]"
+        shuffle=$(cmus-remote -C status | grep "shuffle" | cut -c 13-)
+        repeat=$(cmus-remote -C status | grep "repeat_current" | cut -c 20-)
+        player
 else
         echo
 fi
