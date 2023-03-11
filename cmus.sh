73c1b6b3974d32b4bdfee344cc4f9463cc915fec
gontoblez <nad-git@outlook.com>
2023-03-11 11:24:40 +0300

    Fixed error
    Fixed sytnax error in lines 14 and 15 that was because variables weren't within the first if-statemnt. now variables are within the if-statment

--- cmus.sh
+++ cmus.sh
@@ -8,18 +8,18 @@ player () {
     echo -n "$artist - $song [$minutes1:$seconds1/$minutes2:$seconds2]"
 }
 
-artist=$(echo -n $(cmus-remote -C status | grep "tag artist" | cut -c 12-))
-song=$(echo -n $(cmus-remote -C status | grep title | cut -c 11-))
-position=$(cmus-remote -C status | grep position | cut -c 10-)
-minutes1=$(prepend_zero $(($position / 60)))
-seconds1=$(prepend_zero $(($position % 60)))
-duration=$(cmus-remote -C status | grep duration | cut -c 10-)
-minutes2=$(prepend_zero $(($duration / 60)))
-seconds2=$(prepend_zero $(($duration % 60)))
-shuffle=$(cmus-remote -C status | grep "shuffle" | cut -c 13-)
-repeat=$(cmus-remote -C status | grep "repeat_current" | cut -c 20-)
 
 if [[ $artist = *[!\ ]* ]]; then
+    artist=$(echo -n $(cmus-remote -C status | grep "tag artist" | cut -c 12-))
+    song=$(echo -n $(cmus-remote -C status | grep title | cut -c 11-))
+    position=$(cmus-remote -C status | grep position | cut -c 10-)
+    minutes1=$(prepend_zero $(($position / 60)))
+    seconds1=$(prepend_zero $(($position % 60)))
+    duration=$(cmus-remote -C status | grep duration | cut -c 10-)
+    minutes2=$(prepend_zero $(($duration / 60)))
+    seconds2=$(prepend_zero $(($duration % 60)))
+    shuffle=$(cmus-remote -C status | grep "shuffle" | cut -c 13-)
+    repeat=$(cmus-remote -C status | grep "repeat_current" | cut -c 20-)
     if [[ $shuffle = true ]] || [[ $repeat = true ]]; then
         player && echo -n " ("
         if [[ $shuffle = true ]]; then
